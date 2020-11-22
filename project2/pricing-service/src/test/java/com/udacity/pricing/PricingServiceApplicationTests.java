9b273eb5817427d02e63e026a9ed0626cb80b9e8
Sam Rich <srich@vmware.com>
2020-11-22 14:18:28 -0500

    Added basic test for pricing service

--- project2/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ project2/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -2,19 +2,28 @@ package com.udacity.pricing;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class PricingServiceApplicationTests {
+	@Autowired
+	private MockMvc mock;
 
 	@Test
 	public void contextLoads() {
 	}
 
 	@Test
-	public void testToDo() {
-
+	public void testGetAllPrices() throws Exception {
+		mock.perform(get("/prices")).andExpect(status().isOk());
 	}
 }
