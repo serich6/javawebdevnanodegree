9bf9408366fe5973473ac88da6720d020d3cdd55
Sam Rich <srich@vmware.com>
2020-12-30 01:34:35 -0500

    Stubbed potential unit tests

--- project4/src/test/java/com/example/demo/SareetaApplicationTests.java
+++ project4/src/test/java/com/example/demo/SareetaApplicationTests.java
@@ -9,8 +9,44 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SareetaApplicationTests {
 
+	/*
+	* From rubric: Code coverage of 60% is met through tests.
+	* The tests include sanity and regression test cases and take care of negative tests as well.
+	 */
+
+	
 	@Test
 	public void contextLoads() {
+	}
+
+	public void testFindById() {
+	}
+
+	public void testFindByUserName() {
+	}
+
+	public void testCreateUser() {
+	}
+
+	public void testSubmit() {
+	}
+
+	public void testGetOrdersForUser() {
+	}
+
+	public void testAddTocart() {
+	}
+
+	public void testRemoveFromcart() {
+	}
+
+	public void testGetItems() {
+	}
+
+	public void testGetItemById() {
+	}
+
+	public void testGetItemsByName() {
 	}
 
 }
