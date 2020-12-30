e9bb22f8f620216eb4254e02fc90a9bdefca94f1
Sam Rich <srich@vmware.com>
2020-12-30 01:28:55 -0500

    Followed course demo, adding requests with auth using postman works.

--- project4/src/main/java/com/example/demo/controllers/UserController.java
+++ project4/src/main/java/com/example/demo/controllers/UserController.java
@@ -1,10 +1,13 @@
 package com.example.demo.controllers;
 
 import java.util.Optional;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,12 +24,15 @@ import com.example.demo.model.requests.CreateUserRequest;
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
-	
+	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 	@Autowired
 	private UserRepository userRepository;
 	
 	@Autowired
 	private CartRepository cartRepository;
+
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	@GetMapping("/id/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id) {
@@ -46,6 +52,20 @@ public class UserController {
 		Cart cart = new Cart();
 		cartRepository.save(cart);
 		user.setCart(cart);
+		// make sure the pw and confirm fields match
+		if (!createUserRequest.getPassword().equals(createUserRequest.getConfirmedPassword())) {
+			logger.error("Error: confirm password field doesn't match original password");
+			return ResponseEntity.badRequest().build();
+		}
+		// can add passcode requirements here (ex: length > 4)
+		if (createUserRequest.getPassword().length() < 5) {
+			logger.error("Error: passcode too short, must be at least 5 characters");
+			return ResponseEntity.badRequest().build();
+		}
+
+		// set the password to the encoded passcode instead of raw value
+		// This step should add the saltJ
+		user.setPassword(bCryptPasswordEncoder.encode(createUserRequest.getPassword()));
 		userRepository.save(user);
 		return ResponseEntity.ok(user);
 	}
--- project4/src/main/java/com/example/demo/model/requests/CreateUserRequest.java
+++ project4/src/main/java/com/example/demo/model/requests/CreateUserRequest.java
@@ -6,6 +6,24 @@ public class CreateUserRequest {
 
 	@JsonProperty
 	private String username;
+	private String password;
+	private String confirmedPassword;
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConfirmedPassword() {
+		return confirmedPassword;
+	}
+
+	public void setConfirmedPassword(String confirmedPassword) {
+		this.confirmedPassword = confirmedPassword;
+	}
 
 	public String getUsername() {
 		return username;
